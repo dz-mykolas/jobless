2f3a62b7017a44a911330ebb5d8faf30a82da8f1
Mykolas Dzidzevičius <108366398+dz-mykolas@users.noreply.github.com>
2023-11-21 21:12:38 +0000

    Added database url check

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -4,8 +4,12 @@ use std::env;
 pub async fn connect_sqlx() -> Result<sqlx::PgPool, sqlx::Error> {
     dotenv::dotenv().ok();
 
-    // Connect to the database.
+    // Get the database URL from the .env file.
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    if database_url.contains("placeholder_") {
+        panic!("DATABASE_URL in .env file is still a placeholder. Please update it with your actual database credentials.");
+    }
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
